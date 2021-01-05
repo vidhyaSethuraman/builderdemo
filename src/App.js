eb01ace6e052144c7068f3992234e590259bbadf
unknown <vidhya.1701254@srec.ac.in>
2021-01-05 16:29:56 +0530

    intial8

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     < >
       <Header/>
       <Banner1/>
-      <Testimonials/>
+     
     </>
   );
 }
--- src/components/Banner1.js
+++ src/components/Banner1.js
@@ -43,11 +43,11 @@ function Banner1() {
               </div>
               <a class="carousel-control-prev" href="#homebanner" role="button" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
+                
               </a>
               <a class="carousel-control-next" href="#homebanner" role="button" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
+                
               </a>
             </div>
           </section>
