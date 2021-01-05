82ff019b7c692bcc66ed4338b847d282adc8aadf
unknown <vidhya.1701254@srec.ac.in>
2021-01-05 19:05:15 +0530

    intial9

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import {useState, useEffect} from 'react';
 import Banner1 from './components/Banner1'
 import Header from './components/Header';
 import Testimonials from './components/Testimonials';
-import pic from './images/logo.svg'
-import picc from './images/ios-icon.png'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
 function App() {
 
   
 
   return (
     < >
-      <Header/>
-      <Banner1/>
+     <Router>
+      <Route path="/" component={Header} />
+        <Route path="/about" component={Banner1} />
+     </Router>
      
     </>
   );
